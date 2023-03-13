3ea70dfba907d4dd0508de200933d5f9b4fe7338
blancharSengaVita <blanchar.senga-vita@student.hepl.be>
2023-03-13 18:35:38 +0100

    Sticky nav

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,10 @@
-function debounce(func, wait = 3, immediate = true) {
+//---- Fais apparaitre les images au scroll ----
+//CONSTANTES
+const italics = document.querySelectorAll('.italic');
+
+const delay = 200;
+//FUNCTION
+function debounce(func, wait = 20, immediate = true) {
   let timeout;
   return () => {
     const context = this;
@@ -15,32 +21,59 @@ function debounce(func, wait = 3, immediate = true) {
   };
 }
 
+
 function checkslide(e){
-  console.log(e);
-  for (const image of images) {
-    const slideInAt = (window.scrollY + window.innerHeight) - (image.height / 10)
+  for (const italic of italics) {
+    const goItalicAt = (window.scrollY + window.innerHeight) - (italic.offsetHeight)
+    italic.classList.add("test");
 
+    console.log(goItalicAt);
+    console.log(italic.offsetHeight);
 
-    const isShown = slideInAt > image.offsetTop;
+    const isShown = goItalicAt > italic.offsetTop;
     if(isShown){
-      image.classList.add('active')
+        italic.classList.remove('test');
+
+        setTimeout(() => {
+          italic.classList.add("italic-text");
+        }, delay);
     }else{
-      image.classList.remove('active')
+      // italic.classList.remove("italic-text");
     }
   }
 }
+//BOUCLES
+for (const italic of italics) {
+  italic.classList.remove('italic-text');
+}
 
+//ADDEVENTLISTENER
 window.addEventListener('scroll', debounce(checkslide));
 
-const h2s  = document.querySelectorAll('h2')
-
-const images = document.querySelectorAll('img');
-
+//---- Fais apparaitre les éléments de l'intro l'un après l'autre avec un delai reglable ----
+//CONSTANTES
 const elements = document.querySelectorAll(".cool");
-const delay = 75;
+const delay1 = 75;
 
+//BOUCLES
 elements.forEach((element, index) => {
+  element.classList.add('test')
   setTimeout(() => {
-    element.classList.add("test");
-  }, delay * (index + 1));
+    element.classList.remove("test");
+  }, delay1 * (index + 1));
 });
+
+
+//---- When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar ----
+
+let prevScrollpos = window.scrollY;
+window.addEventListener('scroll', ()=>{
+  const currentScrollPos = window.scrollY;
+  if (prevScrollpos > currentScrollPos) {
+    document.querySelector("nav").style.top = "0";
+  } else {
+    document.querySelector("nav").style.top = "-200px";
+  }
+  prevScrollpos = currentScrollPos;
+});
+
